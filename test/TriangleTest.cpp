f62c4489c5e2700acdc9245a2420a407db5a14a5
Łukasz Seremak <lukasz.seremak@outlook.com>
2017-06-24 17:59:54 +0200

    added test to traingle

---
+++ test/TriangleTest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by user on 24.06.17.
+//
+
+#include <gtest/gtest.h>
+#include "Triangle.h"
+namespace test
+{
+    class Triangletest : public ::testing::Test
+    {
+
+    };
+
+    TEST_F(Triangletest, Simpletest)
+    {
+        figures::Triangle sq;
+    }
+}
