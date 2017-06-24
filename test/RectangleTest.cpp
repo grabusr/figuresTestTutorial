24011f7643ef95e17c23391a897bb3bf18e3c88e
mnajda <225930@student.pwr.edu.pl>
2017-06-24 17:46:20 +0200

    implemented tests for square and rectangle

--- test/RectangleTest.cpp
+++ test/RectangleTest.cpp
@@ -3,10 +3,22 @@
 //
 
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
+#include "Rectangle.h"
+
+using namespace testing;
 
 namespace test {
 
     class RectangleTest : public ::testing::Test {
 
     };
+
+    TEST(RectangleTest, ForArgumentsFiveAndTwoReturnedAreaIsTen) {
+        figures::Rectangle rectangle;
+
+        const auto returnedArea = rectangle.getArea();
+        EXPECT_THAT(returnedArea, FloatEq(10));
+    }
+
 }
--- test/SquareTest.cpp
+++ test/SquareTest.cpp
@@ -1,8 +1,20 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
+#include "Square.h"
+
+using namespace testing;
 
 namespace test {
 
     class SquareTest : public ::testing::Test {
 
     };
+
+    TEST(SquareTest, ForArgumentFiveReturnedAreaIsTwentyFive) {
+        figures::Square square;
+
+        const auto returnedArea = square.getArea();
+
+        EXPECT_THAT(returnedArea, FloatEq(25));
+    }
 }
