5045141fa5ab01ff8c419992eaa989ff296af503
Davikson <davikson1@hotmail.com>
2017-06-24 18:01:10 +0200

    Added test to circle

---
+++ test/CircleTest.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <Circle.h>
+
+namespace test
+{
+	class CircleTest : public ::testing::Test
+	{
+
+	};
+
+	TEST_F(CircleTest, Simpletest)
+	{
+		figures::Circle rc;
+	}
+}
